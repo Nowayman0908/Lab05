e8abf212818c7e815b2da2bb13169f974c835009
None <aydensem2022@gmail.com>
2023-02-21 18:59:49 -0800

    Initial commit

---
+++ app/src/main/java/cs301/Soccer/SoccerDatabase.java
@@ -0,0 +1,251 @@
+package cs301.Soccer;
+
+import android.util.Log;
+import cs301.Soccer.soccerPlayer.SoccerPlayer;
+import java.io.File;
+import java.util.*;
+
+/**
+ * Soccer player database -- presently, all dummied up
+ *
+ * @author *** put your name here ***
+ * @version *** put date of completion here ***
+ *
+ */
+public class SoccerDatabase implements SoccerDB {
+
+    // dummied up variable; you will need to change this
+    private Hashtable <String,SoccerPlayer> database = new Hashtable<>();
+
+    /**
+     * add a player
+     *
+     * @see SoccerDB#addPlayer(String, String, int, String)
+     */
+    @Override
+    public boolean addPlayer(String firstName, String lastName,
+                             int uniformNumber, String teamName) {
+        String key = firstName + "##" + lastName;
+        if(database.containsKey(key)){
+            return false;
+        }
+        else{
+            SoccerPlayer addedPlayer = new SoccerPlayer(firstName,lastName,uniformNumber,teamName);
+            database.put(key,addedPlayer);
+            return true;
+        }
+    }
+
+    /**
+     * remove a player
+     *
+     * @see SoccerDB#removePlayer(String, String)
+     */
+    @Override
+    public boolean removePlayer(String firstName, String lastName) {
+        String funKey = firstName + "##" + lastName;
+        if(database.containsKey(funKey)){
+            database.remove(funKey);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    /**
+     * look up a player
+     *
+     * @see SoccerDB#getPlayer(String, String)
+     */
+    @Override
+    public SoccerPlayer getPlayer(String firstName, String lastName) {
+        String findKey = firstName + "##" + lastName;
+
+        if(database.containsKey(findKey)){
+            return (SoccerPlayer) database.get(findKey);
+        }
+        else {
+            return null;
+        }
+    }
+
+    /**
+     * increment a player's goals
+     *
+     * @see SoccerDB#bumpGoals(String, String)
+     */
+    @Override
+    public boolean bumpGoals(String firstName, String lastName) {
+        String evenFunKey = firstName + "##" + lastName;
+        if(database.containsKey(evenFunKey)){
+            SoccerPlayer goal = (SoccerPlayer) database.get(evenFunKey);
+            goal.bumpGoals();
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    /**
+     * increment a player's yellow cards
+     *
+     * @see SoccerDB#bumpYellowCards(String, String)
+     */
+    @Override
+    public boolean bumpYellowCards(String firstName, String lastName) {
+        String evenFunKey = firstName + "##" + lastName;
+        if(database.containsKey(evenFunKey)){
+            SoccerPlayer goal = (SoccerPlayer) database.get(evenFunKey);
+            goal.bumpYellowCards();
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    /**
+     * increment a player's red cards
+     *
+     * @see SoccerDB#bumpRedCards(String, String)
+     */
+    @Override
+    public boolean bumpRedCards(String firstName, String lastName) {
+        String evenFunKey = firstName + "##" + lastName;
+        if(database.containsKey(evenFunKey)){
+            SoccerPlayer goal = (SoccerPlayer) database.get(evenFunKey);
+            goal.bumpRedCards();
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    /**
+     * tells the number of players on a given team
+     *
+     * @see SoccerDB#numPlayers(String)
+     */
+    @Override
+    // report number of players on a given team (or all players, if null)
+    public int numPlayers(String teamName) {
+        //Checkpoint 4 Complete.
+        int playerCount = 0;
+
+        if(teamName == null){
+            return database.size();
+        }
+        else{
+            String curTeam = "dur";
+            SoccerPlayer current;
+            Enumeration<SoccerPlayer> arr = database.elements();
+            for(int i = 0; arr.hasMoreElements(); i++) {
+                current = arr.nextElement();
+                curTeam = current.getTeamName();
+                if(teamName.equals(curTeam)){
+                    playerCount++;
+                }
+            }
+
+            return playerCount;
+        }
+    }
+
+    /**
+     * gives the nth player on a the given team
+     *
+     * @see SoccerDB#playerIndex(int, String)
+     */
+    // get the nTH player
+    @Override
+    public SoccerPlayer playerIndex(int idx, String teamName) {
+        //In progress.
+        String curTeam = "dur";
+        int teamPlayCount = 0;
+        SoccerPlayer current = null;
+        Enumeration<SoccerPlayer> arr = database.elements();
+        if(numPlayers(teamName) > idx && numPlayers(teamName) != 0){
+            return null;
+        }
+        else{
+            if(teamName == null){
+                for(int i = 0; i < idx; i++) {
+                    current = arr.nextElement();
+                }
+                return getPlayer(current.getFirstName(), current.getLastName());
+            }
+            else{
+
+                for(int i = 0; i < database.size(); i++) {
+                    current = arr.nextElement();
+                    if(current.getTeamName().equals(teamName)){
+                        teamPlayCount++;
+                        if(teamPlayCount >= idx){
+                            return getPlayer(current.getFirstName(), current.getLastName());
+                        }
+                    }
+                }
+                return null;
+            }
+
+        }
+    }
+
+    /**
+     * reads database data from a file
+     *
+     * @see SoccerDB#readData(java.io.File)
+     */
+    // read data from file
+    @Override
+    public boolean readData(File file) {
+        return file.exists();
+    }
+
+    /**
+     * write database data to a file
+     *
+     * @see SoccerDB#writeData(java.io.File)
+     */
+    // write data to file
+    @Override
+    public boolean writeData(File file) {
+        return false;
+    }
+
+    /**
+     * helper method that logcat-logs a string, and then returns the string.
+     * @param s the string to log
+     * @return the string s, unchanged
+     */
+    private String logString(String s) {
+        Log.i("write string", s);
+        return s;
+    }
+
+    /**
+     * returns the list of team names in the database
+     *
+     * @see cs301.Soccer.SoccerDB#getTeams()
+     */
+    // return list of teams
+    @Override
+    public HashSet<String> getTeams() {
+        return new HashSet<String>();
+    }
+
+    /**
+     * Helper method to empty the database and the list of teams in the spinner;
+     * this is faster than restarting the app
+     */
+    public boolean clear() {
+        if(database != null) {
+            database.clear();
+            return true;
+        }
+        return false;
+    }
+}
